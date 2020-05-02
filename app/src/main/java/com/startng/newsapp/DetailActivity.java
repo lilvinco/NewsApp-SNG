e53c1b3132939f6167ce4d2c61b91148f14f6aff
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-02 16:08:43 +0100

    Update

--- app/src/main/java/com/startng/newsapp/DetailActivity.java
+++ app/src/main/java/com/startng/newsapp/DetailActivity.java
@@ -35,9 +35,27 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         switch (item.getItemId()){
+            //respond to Save click
             case R.id.save:
-                Toast.makeText(DetailActivity.this,"Save button", Toast.LENGTH_SHORT).show();
+                //TODO: Work on Save Click
                 break;
+            //respond to Category click
+            case R.id.category:
+                //TODO: Work on Category Click
+                break;
+            //respond to Reminder click
+            case R.id.reminder:
+                //TODO: Work on Reminder Click
+                break;
+            //respond to Delete click
+            case R.id.delete:
+                //TODO: Work on Delete Click
+                break;
+            //respond to Share click
+            case R.id.share:
+                //TODO: Work on Share Click
+                break;
+
         }
 
         return super.onOptionsItemSelected(item);
