eb45968714f916b9454659f5ed6cc59a7cc038a9
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-02 12:07:39 +0100

    Updates

--- app/src/main/java/com/startng/notesapp/WelcomeActivity.java
+++ app/src/main/java/com/startng/notesapp/WelcomeActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class WelcomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -18,6 +20,17 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        ArrayList<Notes> notesArrayList = new ArrayList<>();
+        notesArrayList.add(new Notes("Millionaire at 21","The above quote is from “Delivering a Lecture,” a chapter in Barbara Gross Davis’ classic text Tools for Teaching. That chapter is an excellent resource for learning how to lecture well. See also Davis’ chapter, “Preparing to Teach the Large Lecture Course.”" ));
+
+        notesArrayList.add(new Notes("Mark Zukerberg", "“Lecturing is not simply a matter of standing in front of a class and reciting what you know. The classroom\n" +
+                " lecture is a special form of communication in which voice, gesture, movement, facial expression, and eye\n" +
+                "contact can either complement or detract from the content. No matter what your topic, your delivery and manner of speaking immeasurably influence your students’ attentiveness and learning.”\n" +
+                "The above quote is from “Delivering a Lecture,” a chapter in Barbara Gross Davis’ classic text Tools for Teaching. That chapter is an excellent resource for learning how to lecture well. See also Davis’ chapter, “Preparing to Teach the Large Lecture Course.”\n"));
+
+        notesArrayList.add(new Notes("COVID 19 - Pandemic", "When planning a lecture, keep in mind that you have control or influence over several elements of your classroom:"));
+
+        notesArrayList.add(new Notes("How to make money in 90 days", "– The slides and other visual aids you use can either complement or confuse your verbal message, depending on how you design them. Consider how photos and other images might function as metaphors that make your points more memorable."));
 
         recyclerView = findViewById(R.id.my_recycler_view);
 
@@ -25,14 +38,13 @@ public class WelcomeActivity extends AppCompatActivity {
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
 
-        // use a linear layout manager
-        layoutManager = new StaggeredGridLayoutManager(3, LinearLayoutManager.VERTICAL);
+        //Used in StaggeredGridLayoutManager
+        layoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
-        // specify an adapter (see also next example)
-        String[] myDataset = getResources().getStringArray(R.array.sports_info);
 
-        mAdapter = new HeadlinesAdapter(this, myDataset);
+
+        mAdapter = new NotesAdapter(this, notesArrayList);
         recyclerView.setAdapter(mAdapter);
     }
 
