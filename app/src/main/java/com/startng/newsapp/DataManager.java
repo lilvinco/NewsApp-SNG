bf834f83db9bb2baa997e0932b942b0244a64c25
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-04 00:15:29 +0100

    firebase

---
+++ app/src/main/java/com/startng/newsapp/DataManager.java
@@ -0,0 +1,22 @@
+package com.startng.newsapp;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DataManager {
+    private final static String  COLLECTIONS_APP = "Notes";
+
+    private final static String TITLE_FIELD = "Title";
+    private final static String CONTENT_FIELD = "Content";
+
+    FirebaseFirestore db;
+
+    //TODO: implement writing to DB
+    public void addToDB(){
+
+    }
+
+    //TODO: implement reading from DB
+    public static void readFromDB(){
+
+    }
+}
