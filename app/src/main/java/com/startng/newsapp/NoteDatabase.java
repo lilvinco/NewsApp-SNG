3f1efb12c84a3931580e2c576a4cd0a04bf55546
Jomoflash <jomoflash22@gmail.com>
2020-05-03 14:21:58 +0100

    Added NoteRepository and updated the NoteDatabase by populating default for testing

--- app/src/main/java/com/startng/newsapp/NoteDatabase.java
+++ app/src/main/java/com/startng/newsapp/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.startng.newsapp;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities =  {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -16,8 +19,31 @@ public abstract class NoteDatabase extends RoomDatabase {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
     }
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db) {
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "Description 1"));
+            return null;
+        }
+    }
+
 }
