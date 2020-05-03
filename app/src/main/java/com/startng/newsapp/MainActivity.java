7924b79cacf725d8af404d4040f0f991a1e4f983
Jomoflash <jomoflash22@gmail.com>
2020-05-03 22:00:00 +0100

    Thank God... Spent almost the whole day trying to resolve the gradle build dependency issues for lifecycle extensions. and also made some cleanups!

--- app/src/main/java/com/startng/newsapp/MainActivity.java
+++ app/src/main/java/com/startng/newsapp/MainActivity.java
@@ -1,20 +1,39 @@
 package com.startng.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Find textviews, get the extras and assign both to each other
-        TextView textView = findViewById(R.id.textView3);
-        String headline = getIntent().getStringExtra("headline");
-        textView.setText(headline);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+        Log.d("Main", "got here");
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                adapter.setNotes(notes);
+            }
+        });
     }
 }
