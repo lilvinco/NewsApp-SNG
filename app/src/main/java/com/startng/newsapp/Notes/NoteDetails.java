0babe5d75437046762809bcadedbd1d32326bd09
JAtoms <nzube1994@gmail.com>
2020-05-04 17:35:09 +0100

    Fixed bug on NoteDetails Activity

--- app/src/main/java/com/startng/newsapp/Notes/NoteDetails.java
+++ app/src/main/java/com/startng/newsapp/Notes/NoteDetails.java
@@ -16,6 +16,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.startng.newsapp.R;
 
 public class NoteDetails extends AppCompatActivity {
 
