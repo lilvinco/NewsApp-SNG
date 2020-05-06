d5648534f27f0058215f98dbfe27e3527088822e
SeunSarumi <55332600+SeunSarumi@users.noreply.github.com>
2020-05-06 20:21:02 +0100

    First Commit

--- app/src/main/java/com/startng/newsapp/HeadlinesActivity.java
+++ app/src/main/java/com/startng/newsapp/HeadlinesActivity.java
@@ -6,15 +6,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.HashSet;
+
 public class HeadlinesActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-    int number;
+    int noteId;
+
+    //private RecyclerView recyclerView;
+    //private RecyclerView.Adapter mAdapter;
+    // private RecyclerView.LayoutManager layoutManager;
+    // int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +30,66 @@ public class HeadlinesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_headlines);
 
 
-        recyclerView = findViewById(R.id.my_recycler_view);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Intent intent = getIntent();
+        noteId = intent.getIntExtra("noteId",-1);
+
+        if (noteId != -1){
+
+
+            editText.setText(MainActivity.notes.get(noteId));
+
+        } else {
+
+            MainActivity.notes.add("");
+            noteId = MainActivity.notes.size() -1;
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+
+
+
+
+        }
+
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                MainActivity.notes.set(noteId, String.valueOf(charSequence));
+                MainActivity.arrayAdapter.notifyDataSetChanged();
+
+
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.startng.newsapp", Context.MODE_PRIVATE);
+                HashSet<String> set = new HashSet(MainActivity.notes);
+                sharedPreferences.edit().putStringSet("notes", set).apply();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //recyclerView = findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
-        recyclerView.setHasFixedSize(true);
+        // recyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        //  layoutManager = new LinearLayoutManager(this);
+        // recyclerView.setLayoutManager(layoutManager);
 
         // specify an adapter (see also next example)
-        String[] myDataset = getResources().getStringArray(R.array.sports_info);
-        mAdapter = new HeadlinesAdapter(this, myDataset);
-        recyclerView.setAdapter(mAdapter);
+        //  String[] myDataset = getResources().getStringArray(R.array.sports_info);
+        //mAdapter = new HeadlinesAdapter(this, myDataset);
+        //  recyclerView.setAdapter(mAdapter);
 
     }
 
