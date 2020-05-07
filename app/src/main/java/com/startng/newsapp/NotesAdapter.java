989a6847d3a1250104be574b8a4d97891c5f643c
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-07 15:45:05 +0100

    Updates

--- app/src/main/java/com/startng/newsapp/NotesAdapter.java
+++ app/src/main/java/com/startng/newsapp/NotesAdapter.java
@@ -1,8 +1,35 @@
 package com.startng.newsapp;
-import com.firebase.ui
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 
 public class NotesAdapter extends FirestoreRecyclerAdapter {
 
 
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public NotesAdapter(@NonNull FirestoreRecyclerOptions options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position, @NonNull Object model) {
+
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
 }
