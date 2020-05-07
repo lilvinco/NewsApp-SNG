d6d295595c9ec0a0d3cedae48969229899affaa0
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-07 18:58:29 +0100

    Update NotesAdapter.java

--- app/src/main/java/com/startng/newsapp/NotesAdapter.java
+++ app/src/main/java/com/startng/newsapp/NotesAdapter.java
@@ -1,7 +1,7 @@
 package com.startng.newsapp;
 
+import android.content.Context;
 import android.os.Build;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,40 +10,31 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import java.util.ArrayList;
 
 import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
 
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder> {
 
-public class NotesAdapter extends FirestoreRecyclerAdapter<Notes, NotesAdapter.NotesViewHolder> {
-
-
-    /**
-     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
-     * FirestoreRecyclerOptions} for configuration options.
-     *
-     * @param options
-     */
-    public NotesAdapter(@NonNull FirestoreRecyclerOptions options) {
-        super(options);
-    }
-
-    @Override
-    protected void onBindViewHolder(@NonNull NotesViewHolder holder, int position, @NonNull Notes model) {
-        //holder.setData(model.getNoteTitle(), model.getNoteContent(), position);
-        Log.v("MyApp", ""+model.getNoteTitle());
-        holder.noteTitleTextView.setText(model.getNoteTitle());
-        holder.noteContentTextView.setText(model.getNoteContent());
-    }
+    private ArrayList<Notes> notes, notesArrayList;
+    private LayoutInflater inflater;
+    private Context context;
 
     @NonNull
     @Override
     public NotesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
-        return new NotesViewHolder(view);
+        return null;
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull NotesViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     class NotesViewHolder extends RecyclerView.ViewHolder {
 
@@ -56,6 +47,7 @@ public class NotesAdapter extends FirestoreRecyclerAdapter<Notes, NotesAdapter.N
             noteTitleTextView = itemView.findViewById(R.id.textViewTitle);
             noteContentTextView = itemView.findViewById(R.id.textViewBody);
 
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 noteContentTextView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
             }
@@ -63,19 +55,19 @@ public class NotesAdapter extends FirestoreRecyclerAdapter<Notes, NotesAdapter.N
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    Intent intent = new Intent(context.getApplicationContext(), DetailActivity.class);
-//                    intent.putExtra(DetailActivity.NOTE_TITLE_TAG, notes.get(currentPosition).getNoteTitle());
-//                    intent.putExtra(DetailActivity.NOTE_CONTENT_TAG, notes.get(currentPosition).getNoteContent());
-//                    Log.v("MApp", "Position for Detail Activity " + currentPosition);
-//                    context.startActivity(intent);
+//                Intent intent = new Intent(context.getApplicationContext(),DetailActivity.class);
+//                intent.putExtra(DetailActivity.NOTE_TITLE_TAG, notes.get(currentPosition).getNoteTitle());
+//                intent.putExtra(DetailActivity.NOTE_CONTENT_TAG, notes.get(currentPosition).getNoteContent());
+//                Log.v("MApp", "Position for Detail Activity " + currentPosition);
+//                context.startActivity(intent);
                 }
             });
         }
 
-//        void setData(String title, String content, int position) {
-//            noteContentTextView.setText(content);
-//            noteTitleTextView.setText(title);
-//            currentPosition = position;
-//        }
+        void setData(String title, String content, int position) {
+            noteContentTextView.setText(content);
+            noteTitleTextView.setText(title);
+            currentPosition = position;
+        }
     }
 }
