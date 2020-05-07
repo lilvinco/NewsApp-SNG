5b740f703414661087a59cf7096f3112fa72cd8d
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-07 01:45:33 +0100

    Update

--- app/src/main/java/com/startng/newsapp/DetailActivity.java
+++ app/src/main/java/com/startng/newsapp/DetailActivity.java
@@ -86,7 +86,6 @@ public class DetailActivity extends AppCompatActivity {
         String content = contentEditText.getText().toString();
 
         DataManager.addToDB(new Notes(title, content));
-        WelcomeActivity.refreshView();
         finish();
     }
 }
