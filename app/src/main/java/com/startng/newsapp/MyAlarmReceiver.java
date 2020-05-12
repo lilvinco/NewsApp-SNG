da1a15b8cd7288ceb6230004be205b0f94e8b865
Mbaya Emmanuel Baldwin <emmanuelbaldwin@gmail.com>
2020-05-12 18:59:03 +0100

    Create MyAlarmReceiver.java

---
+++ app/src/main/java/com/startng/newsapp/MyAlarmReceiver.java
@@ -0,0 +1,15 @@
+package com.startng.newsapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("MyApp", "Alarm just fired");
+//        NotificationScheduler.showNotification(context, MainActivity.class,
+//                "You have 5 unwatched videos", "Watch them now?");
+    }
+}
